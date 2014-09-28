1c7826d5b75eec28c03fdfa4b6a8295289b7f615
Olivier Mehani <shtrom@ssji.net>
2014-09-28 23:14:29 +1000

    dh_make -m -f ~/download/PebbleSDK-2.5.tar.gz
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

---
+++ menu.ex
@@ -0,0 +1,2 @@
+?package(pebble-sdk):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="pebble-sdk" command="/usr/bin/pebble-sdk"
---
+++ pebble-sdk.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pebble-sdk initscript
+# sourced by /etc/init.d/pebble-sdk
+# installed at /etc/default/pebble-sdk by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
